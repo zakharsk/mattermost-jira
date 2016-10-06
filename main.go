fe0adc6c665f0edead457a41d1cf1d82df294bdc
Zakhar Skorokhodov <zskorokhodov@game-forest.com>
2016-10-06 18:56:50 +1000

    Update message text

--- main.go
+++ main.go
@@ -1,35 +1,36 @@
 package main
 
 import (
-	"net/http"
-	"os"
-	"encoding/json"
 	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"net/url"
+	"os"
+	"log"
 )
 
 // Get it
 
 type Data struct {
 	WebhookEvent string
-	User User
-	Issue Issue
-}
-
-type User struct {
-	Name string
-	DisplayName string
-}
-
-type Issue struct {
-	Self string
-	Key string
-	Fields Fields
-}
-
-type Fields struct {
-	Summary string
+	User         struct {
+		Name        string
+		DisplayName string
+	}
+	Issue struct {
+		Self   string
+		Key    string
+		Fields struct {
+			Issuetype struct {
+				IconUrl string
+			}
+			Summary string
+		}
+	}
+	Comment struct {
+		Body string
+	}
 }
 
 // Send it
@@ -41,34 +42,49 @@ type Message struct {
 func index(w http.ResponseWriter, r *http.Request) {
 	// Get mattermost URL
 	mattermostHookURL := r.URL.Query().Get("mattermost_hook_url")
-	//log.Println(mattermostHookURL) // FIXME Delete it
 
 	// Parse JSON from JIRA
 	decoder := json.NewDecoder(r.Body)
 	var data Data
 	decoder.Decode(&data)
-	//log.Println(data) // FIXME Delete it
 
 	// Get JIRA URL
-
 	u, _ := url.Parse(data.Issue.Self)
-	//log.Println(u.Scheme + u.Host)
 
-	// Send data to Mattermost
+	// Select action
+	var action, appendix string
+	switch data.WebhookEvent {
+	case "jira:issue_created":
+		action = "created"
+	case "jira:issue_updated":
+		action = "updated"
+	case "jira:issue_deleted":
+		action = "deleted"
+	}
 
-	// Create message
+	if len(data.Comment.Body) > 0 {
+		action = "commented"
+		appendix = fmt.Sprintf(" with\n>%s", data.Comment.Body)
+	}
+
+	// Create message for Mattermost
 	text := fmt.Sprintf(
-		"[[%s] %s](%s://%s/browse/%s) %s by [%s](%s://%s/secure/ViewProfile.jspa?name=%s)",
-		data.Issue.Key,
-		data.Issue.Fields.Summary,
-		u.Scheme,
-		u.Host,
-		data.Issue.Key,
-		data.WebhookEvent,
+		//[UserFirstName UserSecondName](user_link) commented issue [[TSK-158]](issue_link) "Test task" with "Test comment"
+		"[%s](%s://%s/secure/ViewProfile.jspa?name=%s) %s %s [%s](%s://%s/browse/%s) \"%s\"%s",
 		data.User.DisplayName,
 		u.Scheme,
 		u.Host,
 		data.User.Name,
+		action,
+		data.Issue.Fields.Issuetype.IconUrl,
+		data.Issue.Key,
+		u.Scheme,
+		u.Host,
+		data.Issue.Key,
+		data.Issue.Fields.Summary,
+		appendix,
+
+
 	)
 
 	message := Message{
@@ -77,16 +93,23 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 	byteMessage, _ := json.Marshal(message)
 
-	// Create http-client
-	req, _ := http.NewRequest("POST", mattermostHookURL, bytes.NewBuffer(byteMessage))
-	req.Header.Set("Content-Type", "application/json")
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
+	if len(mattermostHookURL) > 0 {
+		// Create http-client
+		req, _ := http.NewRequest("POST", mattermostHookURL, bytes.NewBuffer(byteMessage))
+		req.Header.Set("Content-Type", "application/json")
+
+		// Send data to Mattermost
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+	} else {
+		log.Println(text)
 	}
-	defer resp.Body.Close()
+
 
 }
 
@@ -97,5 +120,5 @@ func main() {
 		port = "5000"
 	}
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":" + port, nil)
+	http.ListenAndServe(":"+port, nil)
 }
