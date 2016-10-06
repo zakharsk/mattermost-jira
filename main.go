2e90fb396342b76ed68571e26964dcc99ab4256e
Zakhar Skorokhodov <zskorokhodov@game-forest.com>
2016-10-06 15:31:31 +1000

    Base connection

--- main.go
+++ main.go
@@ -3,19 +3,66 @@ package main
 import (
 	"net/http"
 	"os"
+	"log"
 	"encoding/json"
+	"bytes"
+	"fmt"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Accept", "application/json")
-	w.Header().Set("Accept-Encoding", "utf-8")
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Content-Encoding", "utf-8")
+type User struct {
+	DisplayName string
+	Self string
+}
 
-	json.NewEncoder(w).Encode("test")
+type Issue struct {
+	User User
+}
+
+type Message struct {
+	Text string
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	// Get mattermost URL
+	mattermostHookURL := r.URL.Query().Get("mattermost_hook_url")
+	log.Println(mattermostHookURL) // FIXME Delete it
+
+	// Parse JSON from JIRA
+	decoder := json.NewDecoder(r.Body)
+	var issue Issue
+	decoder.Decode(&issue)
+	log.Println(issue) // FIXME Delete it
+
+	// Send data to Mattermost
+
+	// Create message
+	text := fmt.Sprintf(
+		"[%s](%s)",
+		issue.User.DisplayName,
+		issue.User.Self,
+	)
+
+	message := Message{
+		Text: text,
+	}
+
+	byteMessage, _ := json.Marshal(message)
+
+	// Create http-client
+	req, _ := http.NewRequest("POST", mattermostHookURL, bytes.NewBuffer(byteMessage))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
 }
 
 func main() {
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "5000"
