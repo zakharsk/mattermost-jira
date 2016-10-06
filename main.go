fd6416580f09d7d297bf2f342430acc4c166547e
Zakhar Skorokhodov <zskorokhodov@game-forest.com>
2016-10-06 13:22:55 +1000

    Update index

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-func index() {
+import (
+	"net/http"
+	"encoding/json"
+)
 
+func index(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Accept", "application/json")
+	w.Header().Set("Accept-Encoding", "utf-8")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Encoding", "utf-8")
+
+	json.NewEncoder(w).Encode("test")
 }
 
 func main() {
-
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":5000", nil)
 }
