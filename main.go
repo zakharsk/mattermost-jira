fa58656151f81c2a4e44bc353113d6cc5d6566be
Zakhar Skorokhodov <zskorokhodov@game-forest.com>
2016-10-06 13:36:18 +1000

    Test simple index

--- main.go
+++ main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	"net/http"
-	"encoding/json"
+	"fmt"
 )
 
-func index(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Accept", "application/json")
-	w.Header().Set("Accept-Encoding", "utf-8")
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Content-Encoding", "utf-8")
-
-	json.NewEncoder(w).Encode("test")
+func index(w http.ResponseWriter, r *http.Request) {
+	//w.Header().Set("Accept", "application/json")
+	//w.Header().Set("Accept-Encoding", "utf-8")
+	//w.Header().Set("Content-Type", "application/json")
+	//w.Header().Set("Content-Encoding", "utf-8")
+	//
+	//json.NewEncoder(w).Encode("test")
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
